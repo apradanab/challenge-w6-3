f62aafc41d17a81bcb631984b11878038cd23242
apradanab <apradanab@gmail.com>
2024-04-18 10:32:52 +0200

    Update package.json

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -8,10 +8,14 @@ export class UsersRouter {
   router = router();
   constructor(private readonly controller: UserController) {
     debug('Instantiated users router');
+
     this.router.get('/', controller.getAll.bind(controller));
     this.router.get('/:id', controller.getById.bind(controller));
-    this.router.post('/', controller.create.bind(controller));
+    this.router.post('/', controller.create.bind(controller));   // .post(/:id,) --> .post(/login) --> .post(/register) :orden correcto
+    // Register : 
     this.router.patch('/:id', controller.update.bind(controller));
+    
     this.router.delete('/:id', controller.delete.bind(controller));
+    // Eviar /delete/:id --> porque rompe el modelo de trabajo de un APIres. Sería así: .delete(/:id,) delete verbo de acción reservado
   }
 }
